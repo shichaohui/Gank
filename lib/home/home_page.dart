3bac6d479cd59ab85f6e3e999daf38bb654cfa84
shichaohui <464206200@qq.com>
2019-09-03 17:21:37 +0800

    Hide debug edge

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(primaryColor: Colors.blue),
       home: _HomePage(),
     );
